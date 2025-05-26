a0367a715c3ce778b95b0fcbcecbd249e8be9f49
doIt-flutterApps <doit.flutterapps@gmail.com>
2025-05-26 16:37:55 +0900

    07-2 [실습] 회원 가입과 로그인 페이지

--- lib/view/intro/intro_page.dart
+++ lib/view/intro/intro_page.dart
@@ -8,6 +8,7 @@ import 'package:honeybee/data/constant.dart';
 import 'package:lottie/lottie.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import '../../data/user.dart';
+import '../auth/auth_page.dart';
 
 class IntroPage extends StatefulWidget {
   const IntroPage({super.key});
@@ -161,6 +162,7 @@ class _IntroPage extends State<IntroPage> {
                 } else {
                   Future.delayed(const Duration(seconds: 2), () {
                     // 회원 가입 페이지로 이동하기
+                    Get.off(const AuthPage());
                     print('로그인 안 됨');
                   });
                 }
